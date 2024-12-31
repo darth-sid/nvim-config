fe809358f701a7323ab4b94a2f275e752da5864d
Siddharth Ajay <sid.ajay05@gmail.com>
2024-12-31 13:51:16 -0800

    telescope fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,8 +3,10 @@ return {
     lazy=true,
     branch='0.1.x',
     dependencies={
-        'nvim-lua/plenary.nvim',
-        'BurntSushi/ripgrep', -- for livegrep
+        {'nvim-lua/plenary.nvim'},
+        {'BurntSushi/ripgrep'}, -- for livegrep
+        { 'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' }
     },
     cmd='Telescope',
     keys={
@@ -22,4 +24,30 @@ return {
             end
         })
     end,
+    config = function()
+        local telescope = require('telescope')
+        telescope.setup{
+            defaults = {
+                file_ignore_patterns = {
+                    '.git/',
+                    'node_modules/',
+                    'build/',
+                },
+                mappings = {
+                    i = {
+                        ['<esc>'] = require('telescope.actions').close,
+                    },
+                },
+            },
+            extensinos = {
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = 'smart_case',
+                },
+            },
+        }
+        telescope.load_extension('fzf')
+    end
 }
