8598fd08bafb0554edae35c9286285fd95697ba1
Siddharth Ajay <sid.ajay05@gmail.com>
2024-12-31 19:11:40 -0800

    use mason for lsps (tofix: volar ts integration)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,5 +3,8 @@ return {
         'vim-python/python-syntax',
         lazy=false,
         ft={'python'},
+        init=function()
+            vim.g.python_highlight_all = 1
+        end,
     },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,76 +1,101 @@
-return {'neovim/nvim-lspconfig',
-    lazy=false,
-    dependencies = {
-        {'ms-jpq/coq_nvim', branch='coq'},
-        {'ms-jpq/coq.artifacts', branch='artifacts'},
-        {'ms-jpq/coq.thirdparty', branch='3p'},
-        {'astral-sh/ruff'},
+return {
+    {
+        'williamboman/mason.nvim',
+        lazy=false,
+        opts={},
     },
-    init=function()
-        vim.g.coq_settings = {
-            keymap = {
-                jump_to_mark='',
-                bigger_preview='',
+    {
+        'williamboman/mason-lspconfig.nvim',
+        lazy=false,
+        opts={
+            ensure_installed = {
+                'pyright', -- advanced python ( type checking, etc )
+                'ruff', -- fast python linting and formatting
+                'ts_ls', -- ts lsp
+                'eslint', -- js/ts linting and formatting
+                'volar', -- vue lsp
             },
-            auto_start=true,
-            clients = { -- custom coq short names
-                ['snippets.short_name'] = 'SNIP',
-                ['buffers.short_name'] = 'BUF',
-                ['lsp.short_name'] = 'LSP',
-                ['lsp_inline.short_name'] = 'COP',
-                ['registers.short_name'] = 'REG',
-                ['tmux.short_name'] = 'TMUX',
+        },
+    },
+    {
+        'neovim/nvim-lspconfig',
+        lazy=false,
+        dependencies = {
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+            {'ms-jpq/coq_nvim', branch='coq'},
+            {'ms-jpq/coq.artifacts', branch='artifacts'},
+            {'ms-jpq/coq.thirdparty', branch='3p'},
+        },
+        init=function()
+            vim.g.coq_settings = {
+                keymap = {
+                    jump_to_mark='',
+                    bigger_preview='',
+                },
+                auto_start=true,
+                clients = { -- custom coq short names
+                    ['snippets.short_name'] = 'SNIP',
+                    ['buffers.short_name'] = 'BUF',
+                    ['lsp.short_name'] = 'LSP',
+                    ['lsp_inline.short_name'] = 'COP',
+                    ['registers.short_name'] = 'REG',
+                    ['tmux.short_name'] = 'TMUX',
+                }
             }
-        }
-    end,
-    config=function()
-        local lsp = require('lspconfig')
-        local coq = require('coq')
-        -- python 
-        -- TODO: clean up pyright ruff coexistence
-        lsp.pyright.setup{coq.lsp_ensure_capabilities{
-            on_attach = function(client, buf)
-                client.server_capabilities.documentFormattingProvider = false
-                client.server_capabilities.documentRangeFormattingProvider = false
-            end
-        }}
-        lsp.ruff.setup{coq.lsp_ensure_capabilities{
-            on_attach = function(client, buf) 
-                client.server_capabilities.documentFormattingProvider = true
-                client.server_capabilities.documentRangeFormattingProvider = true
-                client.server_capabilities.hoverProvider = false
-                client.server_capabilities.definitionProvider = false
-            end
-        }}
-        -- ts & vue
-        -- TODO: verify vue support
-        -- TODO: fix eslint
-        local vue_language_server_path = '/opt/homebrew/lib/node_modules/@vue/language-server'
-        lsp.ts_ls.setup{
-            init_options = {
-                documentFormatting = false,
-                plugins = {
-                    {
-                        name='@vue/typescript-plugin',
-                        location=vue_language_server_path,
-                        languages={'vue'},
+        end,
+        config=function()
+            local lsp = require('lspconfig')
+            local coq = require('coq')
+            -- python 
+            -- TODO: clean up pyright ruff coexistence
+            lsp.pyright.setup{coq.lsp_ensure_capabilities{
+                on_attach = function(client, buf)
+                    client.server_capabilities.documentFormattingProvider = false
+                    client.server_capabilities.documentRangeFormattingProvider = false
+                end
+            }}
+            lsp.ruff.setup{coq.lsp_ensure_capabilities{
+                on_attach = function(client, buf) 
+                    client.server_capabilities.documentFormattingProvider = true
+                    client.server_capabilities.documentRangeFormattingProvider = true
+                    client.server_capabilities.hoverProvider = false
+                    client.server_capabilities.definitionProvider = false
+                end
+            }}
+            -- ts & vue
+            -- TODO: verify vue support
+            -- TODO: fix eslint
+            lsp.ts_ls.setup{
+                init_options = {
+                    documentFormatting = false,
+                },
+                filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact'},
+            }
+            lsp.volar.setup{
+                filetypes = { 'vue' },
+                init_options = {
+                    vue = {
+                        hybridMpde = false,
+                    },
+                    typescript = {
+                        tsdk = '/opt/homebrew/lib/node_modules/typescript/lib',
                     },
                 },
-            },
-            filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-        }
-        lsp.eslint.setup{
-            on_attach = function(client, bufnr)
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    buffer = bufnr,
-                    command = "EslintFixAll",
-                })
-            end,
-        }
-        lsp.volar.setup{}
+            }
 
-        require('coq_3p') {
-            {src = 'bc', short_name = 'MATH', precision = 6} -- scientific calculator
-        }
-    end,
+            lsp.eslint.setup{
+                on_attach = function(client, bufnr)
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
+                        command = "EslintFixAll",
+                    })
+                end,
+            }
+
+            require('coq_3p') {
+                {src = 'bc', short_name = 'MATH', precision = 6} -- scientific calculator
+            }
+        end,
+    }
 }
